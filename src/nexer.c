0d64d1fdd6934a08466d83bc6d825f2e78b36d75
Nolan <njf5272@gmail.com>
2025-05-02 08:23:43 +0000

    revised nexer.c

--- src/nexer.c
+++ src/nexer.c
@@ -27,6 +27,92 @@ void print_token(NolanToken nToken){
 		case TOKEN_SYMBOL: type_str = "Symbol"; break;
 		default: type_str = "Undefined nToken"; break;
 	}
-	printf("Received: %s which is nToken [%s]", nToken.text, type_str);
 
+	//printf("Received: %s which is nToken [%s]", nToken.text, type_str);
+	
+	FILE *output_file = fopen("output.txt", "a");
+    if (!output_file) {
+        fprintf(stderr, "Error: Could not open output file.\n");
+        exit(1);
+    }
+
+    fprintf(output_file, "Received: %s which is nToken [%s]\n", nToken.text, type_str);
+
+    fclose(output_file);
+}
+
+int is_cword(const char *str){
+
+	if(strcmp(str, "int") == 0 || strcmp(str, "return") == 0){
+		return 1;
+	}else{
+		return 0;
+	}
+
+}
+
+int main(){
+
+	FILE *file = fopen("test.c", "r");
+	if (!file){
+		printf("error w/file");
+		return 1;
+	}
+
+	NolanToken nToken;
+	char rChar;
+
+	while ((rChar = fgetc(file)) != EOF){
+		if (isspace(rChar)) continue;
+
+		if (isalpha(rChar) || rChar =='_'){
+			int i = 0;
+			nToken.text[i] = rChar;
+			i++;
+
+			while (isalnum(rChar = fgetc(file)) | rChar == '_'){
+				nToken.text[i] = rChar;
+				i++;
+			}
+
+			nToken.text[i] = '\0';
+			ungetc(rChar, file);
+
+			if (is_cword(nToken.text)){
+				nToken.type = TOKEN_CWORD;
+			}
+			else{
+				nToken.type = TOKEN_UWORD;
+			}
+			
+			print_token(nToken);
+		}
+
+		else if(isdigit(rChar)){
+			int i = 0;
+			nToken.text[i] = rChar;
+			i++;
+			while (isdigit(rChar = fgetc(file))){
+				nToken.text[i] = rChar;
+				i++;
+			}
+			nToken.text[i] = '\0';
+			ungetc(rChar, file);
+
+			nToken.type = TOKEN_NUM;
+			print_token(nToken);
+
+		}
+
+		else if(ispunct(rChar)){
+			nToken.text[0] = rChar;
+			nToken.text[1] = '\0';
+			nToken.type = TOKEN_SYMBOL;
+			print_token(nToken);
+		}
+
+	}
+
+	fclose(file);
+	return 0;
 }
